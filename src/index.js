692c769fd164d335ecc06929ed7e01f008ddb542
blackchapel <kc@protonmail.com>
2022-11-22 12:35:18 +0530

    imported routes in index.js

--- src/index.js
+++ src/index.js
@@ -5,6 +5,9 @@ const fs = require('fs');
 const dontenv = require('dotenv').config();
 const db = require('./configs/connection.config');
 
+// Importing routes
+const authRoutes = require('./routes/auth.route');
+
 // Initializing an express app
 const app = express();
 
@@ -18,6 +21,9 @@ app.use(express.urlencoded({ extended: true }));
 
 // Logging incoming requests
 app.use(morgan('dev'));
+
+// APIs
+app.use('/auth', authRoutes);
 
 // Test API
 app.get('/api', (req, res) => {
